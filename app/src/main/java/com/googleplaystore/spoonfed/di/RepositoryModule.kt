59b2aaecd10b210f9d811986bdb6bcd739238bfd
Jonathan Goris <jonathangoris123@gmail.com>
2022-12-01 15:24:47 -0500

    created RepositoryModule.kt to bind the RecipeRepository to RecipeRepositoryImpl

---
+++ app/src/main/java/com/googleplaystore/spoonfed/di/RepositoryModule.kt
@@ -0,0 +1,23 @@
+package com.googleplaystore.spoonfed.di
+
+import com.googleplaystore.spoonfed.data.remote.service.RecipeService
+import com.googleplaystore.spoonfed.data.repository.RecipeRepositoryImpl
+import com.googleplaystore.spoonfed.domain.repository.RecipeRepository
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object RepositoryModule {
+
+    @Provides
+    @Singleton
+    fun bindRecipeRepository(
+        apiService: RecipeService,
+    ): RecipeRepository {
+        return RecipeRepositoryImpl(apiService)
+    }
+}
