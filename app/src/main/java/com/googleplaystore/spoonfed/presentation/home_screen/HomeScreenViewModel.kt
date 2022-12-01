b99c0a38864b0d829b7f74f6201765f7134ee4cf
Jonathan Goris <jonathangoris123@gmail.com>
2022-12-01 15:19:41 -0500

    Created the file for the home screen state data class to hold api results

---
+++ app/src/main/java/com/googleplaystore/spoonfed/presentation/home_screen/HomeScreenViewModel.kt
@@ -0,0 +1,5 @@
+package com.googleplaystore.spoonfed.presentation.home_screen
+
+//TODO Think about creating use cases for different api calls before initializing this viewmodel
+class HomeScreenViewModel {
+}
