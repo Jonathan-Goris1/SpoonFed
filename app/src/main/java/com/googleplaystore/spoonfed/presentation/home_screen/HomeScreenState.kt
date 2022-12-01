dab8a6c7adf28d4ae18c6e48f004bebf0e90fda9
Jonathan Goris <jonathangoris123@gmail.com>
2022-12-01 15:18:46 -0500

    Created the file for the home screen state data class to hold api results

---
+++ app/src/main/java/com/googleplaystore/spoonfed/presentation/home_screen/HomeScreenState.kt
@@ -0,0 +1,9 @@
+package com.googleplaystore.spoonfed.presentation.home_screen
+
+import com.googleplaystore.spoonfed.domain.models.Recipe
+
+data class HomeScreenState(
+    val isLoading: Boolean = false,
+    val error: String? = null,
+    val recipes: List<Recipe> = emptyList()
+)
