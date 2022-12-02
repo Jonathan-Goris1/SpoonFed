caaa778ed7fe78f96bce7f1fc88dcb213811f4d0
Jonathan Goris <jonathangoris123@gmail.com>
2022-12-02 15:25:47 -0500

    made recipe parameter nullable. added colors parameter to card. added contentScale parameter to image. added padding to text

--- app/src/main/java/com/googleplaystore/spoonfed/presentation/home_screen/components/RecipeCard.kt
+++ app/src/main/java/com/googleplaystore/spoonfed/presentation/home_screen/components/RecipeCard.kt
@@ -7,10 +7,13 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import coil.compose.rememberAsyncImagePainter
@@ -19,28 +22,32 @@ import com.googleplaystore.spoonfed.domain.models.Recipe
 @Composable
 fun RecipeCard(
     modifier: Modifier = Modifier,
-    recipe: Recipe,
+    recipe: Recipe?,
     onClick: () -> Unit
 ) {
 
     Card(
         modifier = modifier
             .padding(0.dp, 8.dp)
-            .clickable(onClick = onClick)
+            .clickable(onClick = onClick),
+        colors = CardDefaults.cardColors(
+            containerColor = Color.Transparent
+        )
     ) {
         Image(
             modifier = modifier
-                .width(100.dp)
-                .height(100.dp)
-                .padding(8.dp)
-                .clip(shape = RoundedCornerShape(1.dp)),
-            painter = rememberAsyncImagePainter(recipe.image),
-            contentDescription = recipe.title,
+                .width(150.dp)
+                .height(150.dp)
+                .clip(shape = RoundedCornerShape(5.dp)),
+            painter = rememberAsyncImagePainter(recipe?.image),
+            contentDescription = recipe?.title,
+            contentScale = ContentScale.FillBounds
         )
 
         Text(
             fontSize = 12.sp,
-            text = recipe.title
+            text = recipe?.title ?: "",
+            modifier = Modifier.padding(horizontal = 8.dp, vertical = 0.dp)
         )
     }
 
