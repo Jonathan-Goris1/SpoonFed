1274c49badb1259b5adf1930ee617be730d5e2a3
Jonathan Goris <jonathangoris123@gmail.com>
2022-12-02 15:19:25 -0500

    created the Home Screen composable and added the search bar

--- app/src/main/java/com/googleplaystore/spoonfed/presentation/home_screen/HomeScreen.kt
+++ app/src/main/java/com/googleplaystore/spoonfed/presentation/home_screen/HomeScreen.kt
@@ -1,3 +1,79 @@
 package com.googleplaystore.spoonfed.presentation.home_screen
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
+import com.googleplaystore.spoonfed.R
+import com.googleplaystore.spoonfed.presentation.home_screen.components.RecipeCard
+
 //TODO create Home Screen composable here
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun HomeScreen(
+    viewModel: HomeScreenViewModel = hiltViewModel()
+){
+    Column(
+        modifier = Modifier
+            .padding(horizontal = 8.dp, vertical = 8.dp
+            ),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+
+
+        Box(modifier = Modifier) {
+            OutlinedTextField(
+                value = viewModel.state.searchQuery,
+                onValueChange = { viewModel.updateSearchQuery(it) },
+                maxLines = 1,
+                singleLine = true,
+                textStyle = TextStyle(color = Color.White),
+                placeholder = { Text("Search for recipes") },
+                leadingIcon = {
+                    Icon(
+                        painter = painterResource(id = R.drawable.ic_baseline_search_24),
+                        contentDescription = stringResource(id = R.string.searchDescription)
+                    )
+                },
+                colors = TextFieldDefaults.textFieldColors(
+                    placeholderColor = Color.LightGray,
+                    containerColor = Color.Transparent
+                ),
+                shape = CircleShape,
+                modifier = Modifier
+                    .fillMaxWidth()
+
+            )
+
+        }
+        LazyColumn {
+            items(viewModel.state.recipes ?: emptyList())  { recipe ->
+                RecipeCard(recipe = recipe) {
+
+                }
+            }
+        }
+    }
+
+}
+
+@Preview
+@Composable
+fun HomeScreenPreview(){
+    HomeScreen()
+}
