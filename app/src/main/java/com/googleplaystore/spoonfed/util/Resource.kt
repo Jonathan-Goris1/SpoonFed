b794b3091227602be3847a63958e0fbc01af4c7c
Jonathan Goris <jonathangoris123@gmail.com>
2022-12-02 15:26:29 -0500

    added a loading class

--- app/src/main/java/com/googleplaystore/spoonfed/util/Resource.kt
+++ app/src/main/java/com/googleplaystore/spoonfed/util/Resource.kt
@@ -6,4 +6,5 @@ sealed class Resource<T>(
 ) {
     class Success<T>(data: T?): Resource<T>(data)
     class Error<T>(message: String, data: T? = null): Resource<T>(data, message)
+    class Loading<T>(val isLoading: Boolean = true): Resource<T>(null)
 }
