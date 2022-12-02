61a9702028cd6e13cafca5251b32e8e607d9ad79
Jonathan Goris <jonathangoris123@gmail.com>
2022-12-02 15:24:50 -0500

    created navigator file to initialize the nav host

---
+++ app/src/main/java/com/googleplaystore/spoonfed/presentation/navigation/Navigator.kt
@@ -0,0 +1,20 @@
+package com.googleplaystore.spoonfed.presentation.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.rememberNavController
+
+@Composable
+fun Navigator(){
+    val navController = rememberNavController()
+    NavHost(
+        navController = navController,
+        startDestination = HOME_ROUTE
+
+    ) {
+
+        homeNavGraph(navController = navController)
+
+    }
+
+}
