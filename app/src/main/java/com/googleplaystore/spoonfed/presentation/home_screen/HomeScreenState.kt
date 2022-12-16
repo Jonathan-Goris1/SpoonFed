966bbea5a37c80568f0685f55d5f38b26f48064d
Jonathan Goris <jonathangoris123@gmail.com>
2022-12-16 17:38:41 -0500

    added a new parameter to check if there is an error

--- app/src/main/java/com/googleplaystore/spoonfed/presentation/home_screen/HomeScreenState.kt
+++ app/src/main/java/com/googleplaystore/spoonfed/presentation/home_screen/HomeScreenState.kt
@@ -3,8 +3,9 @@ package com.googleplaystore.spoonfed.presentation.home_screen
 import com.googleplaystore.spoonfed.domain.models.Recipe
 
 data class HomeScreenState(
-    val isLoading: Boolean = false,
-    val error: String? = null,
+    val isLoading: Boolean = true,
+    val hasError: Boolean= false,
+    val errorMessage: String? = null,
     val recipes: List<Recipe>? = emptyList(),
     val searchQuery: String = ""
 )
