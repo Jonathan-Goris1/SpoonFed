9ec44d816867f705e2c958202076b4457b2bfbff
Jonathan Goris <jonathangoris123@gmail.com>
2022-12-21 17:14:15 -0500

    replaced painter resource with material 3 Icons

--- app/src/main/java/com/googleplaystore/spoonfed/presentation/home_screen/HomeScreen.kt
+++ app/src/main/java/com/googleplaystore/spoonfed/presentation/home_screen/HomeScreen.kt
@@ -7,19 +7,20 @@ import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.googleplaystore.spoonfed.R
-import com.googleplaystore.spoonfed.presentation.home_screen.components.RecipeCard
+import com.googleplaystore.spoonfed.presentation.components.RecipeCard
 
 private const val TAG: String = "HOME_SCREEN"
 
@@ -51,7 +52,7 @@ fun HomeScreen(
             keyboardActions = KeyboardActions { viewModel.getQueryRecipe(query = viewModel.state.searchQuery) },
             leadingIcon = {
                 Icon(
-                    painter = painterResource(id = R.drawable.ic_baseline_search_24),
+                    imageVector = Icons.Filled.Search,
                     contentDescription = stringResource(id = R.string.searchDescription)
                 )
             },
@@ -80,7 +81,6 @@ fun HomeScreen(
         if (viewModel.state.hasError) {
             ErrorState(errorMessage = viewModel.state.errorMessage.toString())
         }
-
     }
 }
 
