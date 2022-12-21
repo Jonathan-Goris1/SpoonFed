9e8bab1a46981221024865175e99ebecbc652104
Jonathan Goris <jonathangoris123@gmail.com>
2022-12-21 17:13:14 -0500

    started creating the detail screen composable

--- app/src/main/java/com/googleplaystore/spoonfed/presentation/detail_screen/DetailScreen.kt
+++ app/src/main/java/com/googleplaystore/spoonfed/presentation/detail_screen/DetailScreen.kt
@@ -1,2 +1,90 @@
 package com.googleplaystore.spoonfed.presentation.detail_screen
 
+import android.annotation.SuppressLint
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.Share
+import androidx.compose.material3.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.unit.dp
+import coil.compose.rememberAsyncImagePainter
+import com.googleplaystore.spoonfed.domain.models.Recipe
+
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun DetailScreen(
+    modifier: Modifier = Modifier,
+    recipe: Recipe?
+) {
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = {
+                    Box(
+                        modifier = Modifier.fillMaxSize(),
+                        contentAlignment = Alignment.Center
+                    ) {
+                        Text(
+                            recipe?.title ?: ""
+                            , color = Color.White
+                        )
+                    }
+                },
+                navigationIcon = {
+                    IconButton(onClick = { }) {
+                        Icon(
+                            imageVector = Icons.Filled.ArrowBack,
+                            contentDescription = "Go Back"
+                        )
+                    }
+                },
+                colors = TopAppBarDefaults.topAppBarColors(
+                    containerColor = Color.Transparent,
+                    scrolledContainerColor = Color.Transparent,
+
+                    ),
+
+                actions = {
+                    IconButton(
+                        modifier = Modifier
+                            .clip(CircleShape)
+                            .background(Color.White)
+                            .size(30.dp, 30.dp),
+                        onClick = { /* doSomething() */ }) {
+                        Icon(
+                            modifier = Modifier.size(20.dp, 20.dp),
+                            imageVector = Icons.Filled.Share,
+                            contentDescription = "Share Recipe"
+                        )
+                    }
+
+
+                }
+            )
+
+
+        }
+    ) {
+        Column() {
+            Image(
+                modifier = modifier
+                    .fillMaxWidth()
+                    .height(100.dp),
+                painter = rememberAsyncImagePainter(recipe?.image),
+                contentDescription = recipe?.title,
+                contentScale = ContentScale.FillBounds
+            )
+        }
+
+    }
+}
