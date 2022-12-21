97683a9f9c2702f09d3211ab534a045a5a618b71
Jonathan Goris <jonathangoris123@gmail.com>
2022-12-21 17:13:40 -0500

    started creating the detail screen viewModel

--- app/src/main/java/com/googleplaystore/spoonfed/presentation/detail_screen/DetailScreenViewModel.kt
+++ app/src/main/java/com/googleplaystore/spoonfed/presentation/detail_screen/DetailScreenViewModel.kt
@@ -1,4 +1,12 @@
 package com.googleplaystore.spoonfed.presentation.detail_screen
 
-class DetailScreenViewModel {
+import androidx.lifecycle.ViewModel
+import com.googleplaystore.spoonfed.domain.repository.RecipeRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class DetailScreenViewModel @Inject constructor(
+    private val repository: RecipeRepository
+) : ViewModel() {
 }
