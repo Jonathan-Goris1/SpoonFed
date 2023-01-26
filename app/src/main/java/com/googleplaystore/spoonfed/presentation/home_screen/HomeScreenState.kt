3cb8cb8a797d616200da318d302163649a756c54
Jonathan Goris <jonathangoris123@gmail.com>
2023-01-26 14:05:47 -0500

    changes isLoading to false

--- app/src/main/java/com/googleplaystore/spoonfed/presentation/home_screen/HomeScreenState.kt
+++ app/src/main/java/com/googleplaystore/spoonfed/presentation/home_screen/HomeScreenState.kt
@@ -3,7 +3,7 @@ package com.googleplaystore.spoonfed.presentation.home_screen
 import com.googleplaystore.spoonfed.domain.models.Recipe
 
 data class HomeScreenState(
-    val isLoading: Boolean = true,
+    val isLoading: Boolean = false,
     val hasError: Boolean= false,
     val errorMessage: String? = null,
     val recipes: List<Recipe>? = emptyList(),
