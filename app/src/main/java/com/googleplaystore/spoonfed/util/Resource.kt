b33aba72213a4e48ccd249c94954beec63d44a43
Jonathan Goris <jonathangoris123@gmail.com>
2023-01-26 14:08:29 -0500

    added a function to see if its successful if not will fallback to an emptyList

--- app/src/main/java/com/googleplaystore/spoonfed/util/Resource.kt
+++ app/src/main/java/com/googleplaystore/spoonfed/util/Resource.kt
@@ -7,3 +7,7 @@ sealed class Resource<T>(
     class Success<T>(data: T?): Resource<T>(data)
     class Error<T>(message: String, data: T? = null): Resource<T>(data, message)
 }
+
+fun <T> Resource<T>.successOr(fallback: T): T {
+    return (this as? Resource.Success<T>)?.data ?: fallback
+}
