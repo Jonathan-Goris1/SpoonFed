687dc3ef8b1007194da7f79a8c4ebda794ca89ea
Jonathan Goris <jonathangoris123@gmail.com>
2023-01-27 16:35:01 -0500

    added arguments and DetailScreen to the composable

--- app/src/main/java/com/googleplaystore/spoonfed/presentation/navigation/HomeNavGraph.kt
+++ app/src/main/java/com/googleplaystore/spoonfed/presentation/navigation/HomeNavGraph.kt
@@ -1,22 +1,33 @@
 package com.googleplaystore.spoonfed.presentation.navigation
 
-import androidx.navigation.NavGraphBuilder
-import androidx.navigation.NavHostController
+import androidx.navigation.*
 import androidx.navigation.compose.composable
-import androidx.navigation.navigation
+import com.googleplaystore.spoonfed.presentation.detail_screen.DetailScreen
 import com.googleplaystore.spoonfed.presentation.home_screen.HomeScreen
 
 fun NavGraphBuilder.homeNavGraph(
     navController: NavHostController
-){
+) {
     navigation(
         startDestination = Screens.Home.route,
         route = HOME_ROUTE
-    ){
+    ) {
         composable(Screens.Home.route) {
-           HomeScreen()
+            HomeScreen(navController = navController)
         }
-        composable(Screens.DetailScreen.route) {
+        composable(
+            route = "${Screens.DetailScreen.route}?recipeId={recipeId}",
+            arguments = listOf(
+                navArgument(name = "recipeId") {
+                    type = NavType.IntType
+                    defaultValue = 0
+                }
+            )
+
+        ) { backStackEntry ->
+            DetailScreen(
+                recipeID = backStackEntry.arguments?.getInt("recipeId"),
+                onNavigateBackToHomeScreen = { navController.popBackStack() })
 
         }
 
