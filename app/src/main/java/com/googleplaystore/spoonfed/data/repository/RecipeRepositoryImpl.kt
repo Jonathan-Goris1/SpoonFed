5548ed0b60321fa82642a857fa0aef107c5c0230
Jonathan Goris <jonathangoris123@gmail.com>
2023-01-27 16:37:18 -0500

    added the suspend function for get a recipe with an id api call

--- app/src/main/java/com/googleplaystore/spoonfed/data/repository/RecipeRepositoryImpl.kt
+++ app/src/main/java/com/googleplaystore/spoonfed/data/repository/RecipeRepositoryImpl.kt
@@ -2,6 +2,7 @@ package com.googleplaystore.spoonfed.data.repository
 
 import com.googleplaystore.spoonfed.data.mapper.toDomainModel
 import com.googleplaystore.spoonfed.data.remote.service.RecipeService
+import com.googleplaystore.spoonfed.domain.models.Recipe
 import com.googleplaystore.spoonfed.domain.models.Recipes
 import com.googleplaystore.spoonfed.domain.repository.RecipeRepository
 import com.googleplaystore.spoonfed.util.Resource
@@ -37,6 +38,25 @@ class RecipeRepositoryImpl@Inject constructor(
     override suspend fun getQueryRecipes(query: String): Resource<Recipes> {
         return try {
             val result = apiService.getQueryRecipes(number = 100, query = query)
+            Resource.Success(result.toDomainModel())
+        } catch (e: IOException){
+            e.printStackTrace()
+            Resource.Error(
+                message = e.message.toString()
+            )
+
+        } catch (e: HttpException){
+            e.printStackTrace()
+            Resource.Error(
+                message = e.message()
+            )
+
+        }
+    }
+
+    override suspend fun getRecipeWithID(recipeID: Int): Resource<Recipe> {
+        return try {
+            val result = apiService.getRecipeByID(id = recipeID)
             Resource.Success(result.toDomainModel())
         } catch (e: IOException){
             e.printStackTrace()
