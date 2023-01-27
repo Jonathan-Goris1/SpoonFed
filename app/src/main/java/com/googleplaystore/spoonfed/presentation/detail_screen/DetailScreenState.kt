55222bc34e62038c5410da209506d1403fc8393f
Jonathan Goris <jonathangoris123@gmail.com>
2023-01-27 16:31:44 -0500

    changed parameter from error to errorMessage added parameter hasError removed searchQuery parameter

--- app/src/main/java/com/googleplaystore/spoonfed/presentation/detail_screen/DetailScreenState.kt
+++ app/src/main/java/com/googleplaystore/spoonfed/presentation/detail_screen/DetailScreenState.kt
@@ -4,7 +4,8 @@ import com.googleplaystore.spoonfed.domain.models.Recipe
 
 data class DetailScreenState(
     val isLoading: Boolean = false,
-    val error: String? = null,
+    val hasError: Boolean= false,
+    val errorMessage: String? = null,
     val recipe: Recipe = Recipe(),
-    val searchQuery: String = ""
+
 )
