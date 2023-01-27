3e99da110214bf9cbf137a661465bb029e7ca631
Jonathan Goris <jonathangoris123@gmail.com>
2023-01-27 16:36:55 -0500

    added the suspend function for get a recipe with an id api call

--- app/src/main/java/com/googleplaystore/spoonfed/domain/repository/RecipeRepository.kt
+++ app/src/main/java/com/googleplaystore/spoonfed/domain/repository/RecipeRepository.kt
@@ -1,5 +1,6 @@
 package com.googleplaystore.spoonfed.domain.repository
 
+import com.googleplaystore.spoonfed.domain.models.Recipe
 import com.googleplaystore.spoonfed.domain.models.Recipes
 import com.googleplaystore.spoonfed.util.Resource
 
@@ -7,4 +8,5 @@ interface RecipeRepository {
 
     suspend fun getRandomRecipes(): Resource<Recipes>
     suspend fun getQueryRecipes(query: String): Resource<Recipes>
+    suspend fun getRecipeWithID(recipeID: Int): Resource<Recipe>
 }
