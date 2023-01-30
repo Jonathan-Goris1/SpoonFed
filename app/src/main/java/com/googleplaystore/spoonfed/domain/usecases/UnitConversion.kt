8845ad9fbcebf200b139edee01dcecd894047936
Jonathan Goris <jonathangoris123@gmail.com>
2023-01-30 14:57:55 -0500

    created a use case just in case i have to edit the amount parameter in ExtendedIngredient to display the fraction

---
+++ app/src/main/java/com/googleplaystore/spoonfed/domain/usecases/UnitConversion.kt
@@ -0,0 +1,6 @@
+package com.googleplaystore.spoonfed.domain.usecases
+
+import com.googleplaystore.spoonfed.domain.repository.RecipeRepository
+
+class UnitConversion(private val repository: RecipeRepository) {
+}
