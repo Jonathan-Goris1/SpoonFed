471a7cd6d9e61995a62981c918bd923727e54281
Jonathan Goris <jonathangoris123@gmail.com>
2023-01-31 16:43:38 -0500

    applied the conversion function on the parameter amount

--- app/src/main/java/com/googleplaystore/spoonfed/data/mapper/ExtendedIngredientMapper.kt
+++ app/src/main/java/com/googleplaystore/spoonfed/data/mapper/ExtendedIngredientMapper.kt
@@ -2,10 +2,11 @@ package com.googleplaystore.spoonfed.data.mapper
 
 import com.googleplaystore.spoonfed.data.remote.responses.ExtendedIngredientResponse
 import com.googleplaystore.spoonfed.domain.models.ExtendedIngredient
+import com.googleplaystore.spoonfed.util.DoubleConvertToFraction
 
 fun ExtendedIngredientResponse.toDomainModel(): ExtendedIngredient = ExtendedIngredient(
     aisle = aisle,
-    amount = amount,
+    amount = DoubleConvertToFraction.convertToFraction(amount ?: 0.0),
     consistency = consistency,
     id = id,
     image = image,
