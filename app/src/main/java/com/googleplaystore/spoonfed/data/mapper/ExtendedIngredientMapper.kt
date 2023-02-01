0cc8e7afeb3a1e294774521e2cae98e2cbaa3b43
Jonathan Goris <jonathangoris123@gmail.com>
2023-02-01 15:18:25 -0500

    removed aisle,consistency,id,image,measures,meta,nameClean,original,originalName because they were not being used

--- app/src/main/java/com/googleplaystore/spoonfed/data/mapper/ExtendedIngredientMapper.kt
+++ app/src/main/java/com/googleplaystore/spoonfed/data/mapper/ExtendedIngredientMapper.kt
@@ -5,16 +5,7 @@ import com.googleplaystore.spoonfed.domain.models.ExtendedIngredient
 import com.googleplaystore.spoonfed.util.DoubleConvertToFraction
 
 fun ExtendedIngredientResponse.toDomainModel(): ExtendedIngredient = ExtendedIngredient(
-    aisle = aisle,
     amount = DoubleConvertToFraction.convertToFraction(amount ?: 0.0),
-    consistency = consistency,
-    id = id,
-    image = image,
-    measures = measures?.toDomainModel(),
-    meta = meta,
     name = name,
-    nameClean = nameClean,
-    original = original,
-    originalName = originalName,
     unit = unit
 )
