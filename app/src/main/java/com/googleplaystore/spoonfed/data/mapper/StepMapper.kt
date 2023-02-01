aa3220c588721380f3c65bab14f07acdc88ac480
Jonathan Goris <jonathangoris123@gmail.com>
2023-02-01 15:23:12 -0500

    removed unused parameters equipment,ingredients,length

--- app/src/main/java/com/googleplaystore/spoonfed/data/mapper/StepMapper.kt
+++ app/src/main/java/com/googleplaystore/spoonfed/data/mapper/StepMapper.kt
@@ -4,9 +4,6 @@ import com.googleplaystore.spoonfed.data.remote.responses.StepResponse
 import com.googleplaystore.spoonfed.domain.models.Step
 
 fun StepResponse.toDomainModel(): Step = Step(
-    equipment = equipment?.map { it.toDomainModel() },
-    ingredients = ingredients?.map { it.toDomainModel() },
-    length = length?.toDomainModel(),
     number = number,
     step = step
 
--- app/src/main/java/com/googleplaystore/spoonfed/domain/models/Step.kt
+++ app/src/main/java/com/googleplaystore/spoonfed/domain/models/Step.kt
@@ -1,9 +1,6 @@
 package com.googleplaystore.spoonfed.domain.models
 
 data class Step(
-    val equipment: List<Equipment>? = emptyList(),
-    val ingredients: List<Ingredient>? = emptyList(),
-    val length: Length? = Length(),
     val number: Int? = 0,
     val step: String? = ""
 )
