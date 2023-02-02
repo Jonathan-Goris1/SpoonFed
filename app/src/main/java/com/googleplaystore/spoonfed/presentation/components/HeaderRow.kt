97c93e8a8069fbba59c1c7bc9a1678af3ee3d1c0
Jonathan Goris <jonathangoris123@gmail.com>
2023-02-02 15:16:36 -0500

    added a onClick parameter to be used on the row composable

--- app/src/main/java/com/googleplaystore/spoonfed/presentation/components/HeaderRow.kt
+++ app/src/main/java/com/googleplaystore/spoonfed/presentation/components/HeaderRow.kt
@@ -1,9 +1,12 @@
 package com.googleplaystore.spoonfed.presentation.components
 
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.style.TextAlign
@@ -12,10 +15,16 @@ import androidx.compose.ui.unit.dp
 @Composable
 fun HeaderRow(
     text1: String = "",
-    text2: String = ""
+    text2: String = "",
+    onClick: () -> Unit = {}
 ){
     Row(
-        modifier = Modifier.padding(8.dp),
+        modifier = Modifier
+            .padding(8.dp)
+            .clickable(
+                interactionSource = remember { MutableInteractionSource() },
+                indication = null
+            ){onClick()},
         verticalAlignment = Alignment.CenterVertically
     ) {
 
