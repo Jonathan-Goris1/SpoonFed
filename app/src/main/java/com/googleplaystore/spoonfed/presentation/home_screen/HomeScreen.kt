83ef0ae86588967fad26b8aaeaadf9ab549ea84d
Jonathan Goris <jonathangoris123@gmail.com>
2023-02-03 16:49:07 -0500

    added a parameter listState to remember the scroll position of the list coroutineScope parameter is for creating a coroutine scope to call listState.animateScrollToItem showButton is a boolean expression that remembers the scroll position and checks whether it is greater than 0 created a scaffold to host the floating action button attached the listState to the LazyVerticalGrid

--- app/src/main/java/com/googleplaystore/spoonfed/presentation/home_screen/HomeScreen.kt
+++ app/src/main/java/com/googleplaystore/spoonfed/presentation/home_screen/HomeScreen.kt
@@ -1,17 +1,17 @@
 package com.googleplaystore.spoonfed.presentation.home_screen
 
 import android.util.Log
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.lazy.grid.GridCells
-import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
-import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.foundation.lazy.grid.*
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.*
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -23,50 +23,79 @@ import androidx.navigation.NavController
 import com.googleplaystore.spoonfed.R
 import com.googleplaystore.spoonfed.domain.models.Recipe
 import com.googleplaystore.spoonfed.presentation.components.RecipeCard
+import com.googleplaystore.spoonfed.presentation.components.ScrollToTopButton
 import com.googleplaystore.spoonfed.presentation.navigation.Screens
+import kotlinx.coroutines.launch
 
 private const val TAG: String = "HOME_SCREEN"
 
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun HomeScreen(
     navController: NavController,
     homeViewModel: HomeScreenViewModel = hiltViewModel()
 ) {
     val state = homeViewModel.uiState.collectAsState().value
-
-    Column(
-        modifier = Modifier
-            .fillMaxWidth()
-            .padding(horizontal = 8.dp, vertical = 8.dp),
-        verticalArrangement = Arrangement.Center
-    ) {
-        Log.d(TAG, "HomeScreen: ${state.isLoading}")
-
-        SearchBar(
-            foodName = state.searchQuery,
-            onFoodNameChange = { homeViewModel.updateSearchQuery(it) },
-            getQueryRecipe = { homeViewModel.getQueryRecipe(state.searchQuery) })
-
-        RecipeItem(recipe = state.recipes, navController = navController)
-
-        if (state.isLoading) {
-            Loading()
-        }
-        if (state.hasError) {
-            ErrorState(errorMessage = state.errorMessage.toString())
+    val listState = rememberLazyGridState()
+    val coroutineScope = rememberCoroutineScope()
+    val showButton by remember {
+        derivedStateOf {
+            listState.firstVisibleItemIndex > 0
         }
     }
+
+
+    Scaffold(
+        floatingActionButton = {
+            AnimatedVisibility(visible = showButton, enter = fadeIn(), exit = fadeOut()) {
+                ScrollToTopButton(goToTop = {
+                    coroutineScope.launch {
+                        listState.animateScrollToItem(0)
+                    }
+                })
+
+            }
+        }
+    ) { paddingValues ->
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(paddingValues),
+            verticalArrangement = Arrangement.Center
+        ) {
+            Log.d(TAG, "HomeScreen: ${state.isLoading}")
+
+            SearchBar(
+                foodName = state.searchQuery,
+                onFoodNameChange = { homeViewModel.updateSearchQuery(it) },
+                getQueryRecipe = { homeViewModel.getQueryRecipe(state.searchQuery) })
+
+            RecipeItem(recipe = state.recipes, navController = navController, listState = listState)
+
+            if (state.isLoading) {
+                Loading()
+            }
+            if (state.hasError) {
+                ErrorState(errorMessage = state.errorMessage.toString())
+            }
+        }
+    }
+
+
 }
 
 @Composable
 fun RecipeItem(
     navController: NavController,
-    recipe: List<Recipe>?
+    recipe: List<Recipe>?,
+    listState: LazyGridState
 ) {
 
     LazyVerticalGrid(
+        state = listState,
         columns = GridCells.Adaptive(minSize = 128.dp),
+        contentPadding = PaddingValues(8.dp)
     ) {
         items(recipe ?: emptyList()) { recipe ->
             RecipeCard(recipe = recipe) { navController.navigate(Screens.DetailScreen.route + "?recipeId=${recipe.id}") }
