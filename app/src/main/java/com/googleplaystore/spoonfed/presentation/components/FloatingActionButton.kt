93a1d70b364267d16d184dafc01250cee8af694b
Jonathan Goris <jonathangoris123@gmail.com>
2023-02-10 16:36:53 -0500

    added color to the floating action button

--- app/src/main/java/com/googleplaystore/spoonfed/presentation/components/FloatingActionButton.kt
+++ app/src/main/java/com/googleplaystore/spoonfed/presentation/components/FloatingActionButton.kt
@@ -6,11 +6,14 @@ import androidx.compose.material.icons.filled.KeyboardArrowUp
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
 
 @Composable
 fun ScrollToTopButton(goToTop: () -> Unit){
     FloatingActionButton(
         shape = CircleShape,
+        containerColor = Color.Black,
+        contentColor = Color.Red,
         onClick = { goToTop() }
     ) {
         Icon(imageVector = Icons.Filled.KeyboardArrowUp , contentDescription = "Back to top")
