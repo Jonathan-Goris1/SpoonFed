f2a450bed33a35ee3901165b126b98012e1d07e8
Jonathan Goris <jonathangoris123@gmail.com>
2023-02-23 16:49:01 -0500

    created new module to inject coroutine scope into the repository created nia dispatcher for IODispatcher annotation

---
+++ app/src/main/java/com/googleplaystore/spoonfed/di/niaDispatcher.kt
@@ -0,0 +1,12 @@
+package com.googleplaystore.spoonfed.di
+
+
+import javax.inject.Qualifier
+
+@Qualifier
+@Retention(AnnotationRetention.RUNTIME)
+annotation class Dispatcher(val niaDispatcher: NiaDispatchers)
+
+enum class NiaDispatchers {
+    IO,
+}
