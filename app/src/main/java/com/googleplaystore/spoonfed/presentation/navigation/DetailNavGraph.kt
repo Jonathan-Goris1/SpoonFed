e4ecadc8f265a05d199ba29076e2e8bdc5610088
Jonathan Goris <jonathangoris123@gmail.com>
2023-02-28 15:16:45 -0500

    created a separate nav graph for details screen and it will be nested with home nav graph

---
+++ app/src/main/java/com/googleplaystore/spoonfed/presentation/navigation/DetailNavGraph.kt
@@ -0,0 +1,33 @@
+package com.googleplaystore.spoonfed.presentation.navigation
+
+import androidx.navigation.NavController
+import androidx.navigation.NavGraphBuilder
+import androidx.navigation.NavType
+import androidx.navigation.compose.composable
+import androidx.navigation.navArgument
+import com.googleplaystore.spoonfed.presentation.detail_screen.DetailRoute
+
+internal const val recipeIdArg = "recipeId"
+
+fun NavController.navigateToDetail(recipeId: Int) {
+    this.navigate("${Screens.DetailScreen.route}?recipeId={$recipeId}")
+}
+
+
+fun NavGraphBuilder.detailScreen(
+    onBackClick: () -> Unit,
+) {
+    composable(
+        route = "${Screens.DetailScreen.route}?recipeId={$recipeIdArg}",
+        arguments = listOf(
+            navArgument(recipeIdArg) {
+                type = NavType.IntType
+                defaultValue = 0
+            }
+        )
+
+    ) {
+        DetailRoute(onBackClick = onBackClick)
+    }
+
+}
