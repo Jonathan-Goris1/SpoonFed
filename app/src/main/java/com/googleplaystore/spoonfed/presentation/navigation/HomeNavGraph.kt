f06015feaeb05ad7c394bbbe812cf8511a27f464
Jonathan Goris <jonathangoris123@gmail.com>
2023-02-28 15:20:04 -0500

    separated detail screen into its own nav graph

--- app/src/main/java/com/googleplaystore/spoonfed/presentation/navigation/HomeNavGraph.kt
+++ app/src/main/java/com/googleplaystore/spoonfed/presentation/navigation/HomeNavGraph.kt
@@ -1,34 +1,23 @@
 package com.googleplaystore.spoonfed.presentation.navigation
 
-import androidx.navigation.*
+import androidx.navigation.NavGraphBuilder
 import androidx.navigation.compose.composable
-import com.googleplaystore.spoonfed.presentation.detail_screen.DetailScreen
-import com.googleplaystore.spoonfed.presentation.home_screen.HomeScreen
+import androidx.navigation.navigation
+import com.googleplaystore.spoonfed.presentation.home_screen.HomeRoute
+
 
 fun NavGraphBuilder.homeNavGraph(
-    navController: NavHostController
+    onRecipeClick: (Int) -> Unit,
+    nestedGraphs: NavGraphBuilder.() -> Unit,
 ) {
     navigation(
         startDestination = Screens.Home.route,
         route = HOME_ROUTE
     ) {
         composable(Screens.Home.route) {
-            HomeScreen(navController = navController)
+            HomeRoute(onRecipeClick)
         }
-        composable(
-            route = "${Screens.DetailScreen.route}?recipeId={recipeId}",
-            arguments = listOf(
-                navArgument(name = "recipeId") {
-                    type = NavType.IntType
-                    defaultValue = 0
-                }
-            )
-
-        ) {
-            DetailScreen(
-                onNavigateBackToHomeScreen = { navController.popBackStack() })
-
-        }
+        nestedGraphs()
 
     }
 
