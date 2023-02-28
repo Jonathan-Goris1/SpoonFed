5b2bbe12bc6d6b9e8cbfeac2290e7535cb40494e
Jonathan Goris <jonathangoris123@gmail.com>
2023-02-28 15:20:50 -0500

    add a navController variable

--- app/src/main/java/com/googleplaystore/spoonfed/MainActivity.kt
+++ app/src/main/java/com/googleplaystore/spoonfed/MainActivity.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
+import androidx.navigation.compose.rememberNavController
 import com.googleplaystore.spoonfed.presentation.navigation.Navigator
 import com.googleplaystore.spoonfed.ui.theme.SpoonFedTheme
 import dagger.hilt.android.AndroidEntryPoint
@@ -22,7 +23,8 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Navigator()
+                    val navController = rememberNavController()
+                    Navigator(navController)
                 }
             }
         }
