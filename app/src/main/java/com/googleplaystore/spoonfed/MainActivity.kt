e53839e7c7e12403c05462f07b00a86bea5a0bf5
Jonathan Goris <jonathangoris123@gmail.com>
2023-03-15 14:59:04 -0400

    added RequiredApi annotation

--- app/src/main/java/com/googleplaystore/spoonfed/MainActivity.kt
+++ app/src/main/java/com/googleplaystore/spoonfed/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.googleplaystore.spoonfed
 
+import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.annotation.RequiresApi
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -14,6 +16,7 @@ import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+    @RequiresApi(Build.VERSION_CODES.M)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
