d4e37e6fedf5566b6473bedba9cc53f82de0e8a7
Jonathan Goris <jonathangoris123@gmail.com>
2023-03-15 14:58:01 -0400

    added RequiredApi annotation

--- app/src/main/java/com/googleplaystore/spoonfed/presentation/navigation/AppNavHost.kt
+++ app/src/main/java/com/googleplaystore/spoonfed/presentation/navigation/AppNavHost.kt
@@ -1,11 +1,14 @@
 package com.googleplaystore.spoonfed.presentation.navigation
 
+import android.os.Build
 import android.util.Log
+import androidx.annotation.RequiresApi
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 
 private const val TAG = "NavHost"
+@RequiresApi(Build.VERSION_CODES.M)
 @Composable
 fun AppNavHost(
     navController: NavHostController
--- app/src/main/java/com/googleplaystore/spoonfed/presentation/navigation/DetailNavGraph.kt
+++ app/src/main/java/com/googleplaystore/spoonfed/presentation/navigation/DetailNavGraph.kt
@@ -1,31 +1,29 @@
 package com.googleplaystore.spoonfed.presentation.navigation
 
+import android.os.Build
 import android.util.Log
+import androidx.annotation.RequiresApi
 import androidx.compose.ui.Modifier
-import androidx.lifecycle.SavedStateHandle
-import androidx.navigation.NavController
-import androidx.navigation.NavGraphBuilder
-import androidx.navigation.NavType
+import androidx.navigation.*
 import androidx.navigation.compose.composable
-import androidx.navigation.navArgument
 import com.googleplaystore.spoonfed.presentation.detail_screen.DetailRoute
 
-const val recipeIdArg = "recipeId"
+
 private const val TAG = "detailNavGraph"
 fun NavController.navigateToDetail(recipeId: Int) {
-    Log.d(TAG, "navigateToDetail: $recipeId")
     this.navigate("${Screens.DetailScreen.route}?recipeId={$recipeId}")
+    Log.d(TAG, "navigateToDetail: $recipeId")
 }
 
 
-
+@RequiresApi(Build.VERSION_CODES.M)
 fun NavGraphBuilder.detailNavGraph(
     onBackClick: () -> Unit,
 ) {
     composable(
-        route = "${Screens.DetailScreen.route}?recipeId={$recipeIdArg}",
+        route = "${Screens.DetailScreen.route}?recipeId={recipeId}",
         arguments = listOf(
-            navArgument(recipeIdArg) {
+            navArgument("recipeId") {
                 type = NavType.IntType
                 defaultValue = 649977
             }
